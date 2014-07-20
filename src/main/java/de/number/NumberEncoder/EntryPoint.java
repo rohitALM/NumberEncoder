f7a50b650ea579314730cfe7a99a0b4a1aeefba2
Rohit Gupta <rohit.gupta@hs-furtwangen.de>
2014-07-20 22:57:47 +0200

    Initial

---
+++ src/main/java/de/number/NumberEncoder/EntryPoint.java
@@ -0,0 +1,15 @@
+package de.number.NumberEncoder;
+
+/**
+ * Hello world!
+ * 
+ */
+public class EntryPoint {
+	public static void main(String[] args) {
+		
+
+		
+		DictionaryReader reader = new DictionaryReader();
+		reader.readFile();
+	}
+}
